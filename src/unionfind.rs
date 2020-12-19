41ca159c09aafd4c40b27a4fddcdbe99fd9bfd28
cupro <cupro.m454@gmail.com>
2020-12-19 20:06:10 +0900

    added

---
+++ src/unionfind.rs
@@ -0,0 +1,61 @@
+pub struct UnionFind {
+    n: usize,
+    parent_or_size: Vec<i32>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> Self {
+        Self {
+            n: size,
+            parent_or_size: vec![-1; size],
+        }
+    }
+    pub fn merge(&mut self, a: usize, b: usize) -> usize {
+        assert!(a < self.n);
+        assert!(b < self.n);
+        let mut x = self.leader(a);
+        let mut y = self.leader(b);
+        if x == y {
+            return x;
+        }
+        if self.parent_or_size[x] > self.parent_or_size[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.parent_or_size[x] += self.parent_or_size[y];
+        self.parent_or_size[y] = x as i32;
+        x
+    }
+    pub fn same(&mut self, a: usize, b: usize) -> bool {
+        assert!(a < self.n);
+        assert!(b < self.n);
+        self.leader(a) == self.leader(b)
+    }
+    pub fn size(&mut self, a: usize) -> usize {
+        assert!(a < self.n);
+        let parent = self.leader(a);
+        -self.parent_or_size[parent] as usize
+    }
+    pub fn leader(&mut self, a: usize) -> usize {
+        assert!(a < self.n);
+        if self.parent_or_size[a] < 0 {
+            a
+        } else {
+            let res = self.leader(self.parent_or_size[a] as usize);
+            self.parent_or_size[a] = res as i32;
+            res
+        }
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::UnionFind;
+
+//     #[test]
+//     fn test_union_find() {
+//         let mut uf = UnionFind::new(10);
+//         uf.merge(1, 2);
+//         uf.merge(2, 3);
+//         assert_eq!(uf.same(1, 3), true);
+//     }
+// }
