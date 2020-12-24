7f406bf0cabfe8297de839cc79c34f870b7abe10
cupro <cupro.m454@gmail.com>
2020-12-24 22:01:48 +0900

    Update

--- src/segtree.rs
+++ src/segtree.rs
@@ -1,75 +1,125 @@
-pub trait Monoid {
-    type S: Clone;
-    fn id() -> Self::S;
-    fn op(a: &Self::S, b: &Self::S) -> Self::S;
-}
-
-pub struct Segtree<M: Monoid> {
+pub struct Segtree<T, F> {
     n: usize,
     sz: usize,
-    lg: usize,
-    d: Vec<M::S>,
+    d: Vec<T>,
+    op: F,
+    id: T,
 }
-impl<M: Monoid> Segtree<M> {
-    pub fn new(size: usize) -> Self {
-        let mut log = 0;
-        while 1 << log < size {
-            log += 1;
+impl<T: Copy, F> Segtree<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new(n: usize, id: T, op: F) -> Self {
+        let mut lg = 0;
+        while 1 << lg < n {
+            lg += 1;
         }
         Self {
-            n: size,
-            sz: 1 << log,
-            lg: log,
-            d: vec![M::id(); 2 << log],
+            n,
+            sz: 1 << lg,
+            d: vec![id; 2 << lg],
+            op,
+            id,
         }
     }
-    pub fn set(&mut self, p: usize, x: M::S) {
+    pub fn set(&mut self, p: usize, x: T) {
         assert!(p < self.n);
-        let p = p + self.sz;
-        self.d[p] = x;
-        for i in 0..self.lg {
-            self.update(p >> 1 + i);
+        let mut q = p + self.sz;
+        self.d[q] = x;
+        while q >> 1 > 0 {
+            q >>= 1;
+            self.update(q);
         }
     }
-    pub fn get(&self, p: usize) -> M::S {
+    pub fn get(&self, p: usize) -> T {
         assert!(p < self.n);
         self.d[p + self.sz].clone()
     }
-    pub fn prod(&self, left: usize, right: usize) -> M::S {
+    pub fn prod(&self, left: usize, right: usize) -> T {
         assert!(left <= right);
         assert!(right <= self.n);
-        let mut sml = M::id();
-        let mut smr = M::id();
+        let mut sml = self.id;
+        let mut smr = self.id;
         let mut l = left + self.sz;
         let mut r = right + self.sz;
         while l < r {
             if l & 1 == 1 {
-                sml = M::op(&sml, &self.d[l]);
+                sml = (self.op)(sml, self.d[l]);
                 l += 1;
             }
             if r & 1 == 1 {
                 r -= 1;
-                smr = M::op(&self.d[r], &smr);
+                smr = (self.op)(self.d[r], smr);
             }
             l >>= 1;
             r >>= 1;
         }
-        M::op(&sml, &smr)
+        (self.op)(sml, smr)
     }
-    pub fn all_prod(&self) -> M::S {
+    pub fn all_prod(&self) -> T {
         self.d[1].clone()
     }
-    fn update(&mut self, k: usize) {
-        self.d[k] = M::op(&self.d[k * 2], &self.d[k * 2 + 1]);
+    pub fn max_right(&self, left: usize, func: impl Fn(T) -> bool) -> usize {
+        assert!(left <= self.n);
+        assert!(func(self.id));
+        if left == self.n {
+            return self.n;
+        }
+        let mut l = left + self.sz;
+        let mut sm = self.id;
+        loop {
+            while l % 2 == 0 {
+                l >>= 1;
+            }
+            if !func((self.op)(sm, self.d[l])) {
+                while l < self.sz {
+                    l = 2 * l;
+                    if func((self.op)(sm, self.d[l])) {
+                        sm = (self.op)(sm, self.d[l]);
+                        l += 1;
+                    }
+                }
+                return l - self.sz;
+            }
+            sm = (self.op)(sm, self.d[l]);
+            l += 1;
+            if (l & l.wrapping_neg()) == l {
+                break;
+            }
+        }
+        self.n
     }
-}
-
-#[cfg(tests)]
-mod tests {
-    use super::Segtree;
-
-    #[test]
-    fn test_segtree() {
-        let mut t = Segtree::new(10);
+    pub fn min_left(&self, right: usize, func: impl Fn(T) -> bool) -> usize {
+        assert!(right <= self.n);
+        assert!(func(self.id));
+        if right == 0 {
+            return 0;
+        }
+        let mut r = right + self.sz;
+        let mut sm = self.id;
+        loop {
+            r -= 1;
+            while r > 1 && r % 2 == 0 {
+                r >>= 1;
+            }
+            if !func((self.op)(self.d[r], sm)) {
+                while r < self.sz {
+                    r = 2 * r + 1;
+                    if func((self.op)(self.d[r], sm)) {
+                        sm = (self.op)(self.d[r], sm);
+                        r -= 1;
+                    }
+                }
+                return r + 1 - self.sz;
+            }
+            sm = (self.op)(self.d[r], sm);
+            if (r & r.wrapping_neg()) == r {
+                break;
+            }
+        }
+        0
+    }
+    fn update(&mut self, k: usize) {
+        self.d[k] = (self.op)(self.d[k * 2], self.d[k * 2 + 1]);
     }
 }
