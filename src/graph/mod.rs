c598c8a60a576a6a7fdfd6dec82d7f02cf517d37
shiro <cupro.m454@gmail.com>
2021-07-03 18:47:19 +0900

    add

---
+++ src/graph/mod.rs
@@ -0,0 +1,15 @@
+pub mod adj_list_graph;
+pub mod shortest_path;
+
+pub trait Graph<'a, E: 'a + DirectedEdge> {
+    type GraphDIter: Iterator<Item = &'a E>;
+    fn size(&self) -> usize;
+    fn adjacencies(&'a self, v: usize) -> Self::GraphDIter;
+}
+
+pub trait DirectedEdge {
+    fn to(&self) -> usize;
+}
+pub trait WeigtedEdge<T>: DirectedEdge {
+    fn cost(&self) -> T;
+}
